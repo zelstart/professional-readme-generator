c0dee6e75da5aebcb187a9dcc4f190e30826430f
zelstart <e.pomeroy95@gmail.com>
2023-08-28 14:12:19 -0400

    badge is now generated

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-function createDoc({ projectName, desc, install, usage, contribution, license, tests, questions, email, github}) {
-return `
+function createDoc({ projectName, desc, install, usage, contribution, license, badge, tests, questions, email, github }) {
+    return `
 # ${projectName}
+${badge}
 
 ## Description
 ${desc}
@@ -40,91 +41,117 @@ ${questions}
 
 ## Contact Me
 [GitHub](https://github.com/${github})  
-You can email me at @{email}  
+You can email me at ${email}  
 
 `
 }
 
+let licenseBadges = [
+    {
+        license: "Apache 2.0",
+        badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    },
+    {
+        license: "Boost Software License 1.0",
+        badge: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    },
+    {
+        license: "Creative Commons (CC0)",
+        badge: "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)",
+    },
+    {
+        license: "ISC",
+        badge: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    },
+    {
+        license: "MIT",
+        badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    },
+]
+
+
+
 // TODO: Create a function to initialize app
 function init() {
-inquirer
-    .prompt([
-        {
-            type: 'input', 
-            name: 'projectName',
-            message: "What is the name of your project?"
-        },
-        {
-            type: 'input' , 
-            name: 'desc',
-            message: "Write a description for your project"
-        },
-        {
-            type: 'input', 
-            name: 'install',
-            message: "If your project has any instructions regarding installation, please provide them here:"
-        },
-        {
-            type: 'input', 
-            name: 'usage',
-            message: "If your project has any usage information, please provide it here:"
-        },
-        {
-            type: 'input', 
-            name: 'contribution',
-            message: "If your project has any contribution guidelines, please describe them here:"
-        },
-        {
-            type: 'list', 
-            name: 'license',
-            message: "What License are you using?",
-            choices: [
-                'MIT',
-                'Academic Free License v3.0',
-                'Artistic License',
-                'Creative Commons',
-                'Apache License',
-                'ISC'
-            ]
-        },
-        {
-            type: 'input', 
-            name: 'tests',
-            message: "Include any tests here:"
-        },
-        {
-            type: 'input', 
-            name: 'questions',
-            message: "Include any questions here:"
-        },
-        {
-            type:'input' , 
-            name: 'github',
-            message: "What is your GitHub username?"
-        },
-        {
-            type: 'input', 
-            name: 'email',
-            message: "What is your email address?"
-        },
-    ])
-    .then((answers) => {
-        const mdContent = createDoc(answers);
-        writeToFile('README.md', mdContent);
-    })
-    .catch((error) => {
-        console.error('An error occurred:', error);
-    });
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: "What is the name of your project?"
+            },
+            {
+                type: 'input',
+                name: 'desc',
+                message: "Write a description for your project"
+            },
+            {
+                type: 'input',
+                name: 'install',
+                message: "If your project has any instructions regarding installation, please provide them here:"
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: "If your project has any usage information, please provide it here:"
+            },
+            {
+                type: 'input',
+                name: 'contribution',
+                message: "If your project has any contribution guidelines, please describe them here:"
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: "What License are you using?",
+                choices: [
+                    'Apache 2.0',
+                    'Boost Software License 1.0',
+                    'Creative Commons (CC0)',
+                    'ISC',
+                    'MIT'
+                ]
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: "Include any tests here:"
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: "Include any questions here:"
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is your GitHub username?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is your email address?"
+            },
+        ])
+        .then((answers) => {
+            const selectedLicense = answers.license;
+            const selectedBadge = licenseBadges.find(item => item.license === selectedLicense).badge;
+            const mdContent = createDoc({ ...answers, badge: selectedBadge }); 
+            writeToFile('generatedREADME.md', mdContent);
+        })
+        .catch((error) => {
+            console.error('An error occurred:', error);
+        });
 }
 
 function writeToFile(fileName, data) {
-fs.writeFile(fileName, data, (err) => {
-    if (err) {
-        console.error('Error writing README:', err);
-    } else {
-        console.log('README created successfully.');
-    }
-});
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing README:', err);
+        } else {
+            console.log('README created successfully.');
+        }
+    });
 }
 
 init();
